fb0dff99f8e7dcd21efae920a7eb833bfacc03a7
Apurba <palapurba2004@gmail.com>
2026-03-15 16:11:08 +0530

    chnages the URL for testing

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://pms-backend-b1t3.onrender.com/api",
   withCredentials: true, // important for cookie auth
 });
 
